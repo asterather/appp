b8f5c28b8da412ddcb31ef677de2a38f2cee0f28
asterather <asterather@gmail.com>
2014-08-14 14:09:45 -0500

    Added welcome resources set welcome/show as root -Heroku is working. It did not have a default location, that is why it was 404. This should fix that.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+root :to => "welcome#show"
   resources :microposts
 
   resources :users
+
+  resources :welcome
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
